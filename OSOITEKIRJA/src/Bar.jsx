f237e1f4f0915d3c0e4b404a7441c0b154d3b204
AbuAk1 <bgh553@myy.haaga-helia.fi>
2024-10-22 14:47:23 +0300

    Component hiararchy and dependency installation

---
+++ OSOITEKIRJA/src/Bar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Appbar } from 'react-native-paper'
+
+export default function Bar() {
+
+  return (
+    <Appbar>
+        <Appbar.Content title="My Places"/>
+    </Appbar>
+  )
+}
